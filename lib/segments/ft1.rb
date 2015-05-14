22556294ddc5ddef6b3d713bbb136d72bb11d56a
TheraVid <jptingle@post.harvard.edu>
2015-05-14 16:48:59 +0000

    added new segements for DG1 and FT1

---
+++ lib/segments/ft1.rb
@@ -0,0 +1,11 @@
+# encoding: UTF-8
+# via https://github.com/bbhoss/ruby-hl7/blob/master/lib/segments/in1.rb
+require 'ruby-hl7'
+class HL7::Message::Segment::FT1 < HL7::Message::Segment
+  add_field :set_id
+  add_field :insurance_plan_id
+  add_field :insurance_company_id
+  add_field :insurance_company_name
+  add_field :insurance_company_address
+  add_field :insurance_company_contact_person
+end
